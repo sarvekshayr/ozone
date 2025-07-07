fdae961ae39199d1d4dadbe9e6c4e25b83a1bd55
sarvekshayr <sarvekshayr@cloudera.com>
2025-07-07 10:48:37 +0530

    Testing tools suite

--- hadoop-ozone/dist/src/main/compose/ozonesecure-ha/test-debug-tools.sh
+++ hadoop-ozone/dist/src/main/compose/ozonesecure-ha/test-debug-tools.sh
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#suite:HA-secure
+#suite:tools
 
 set -u -o pipefail
 
@@ -38,6 +38,9 @@ export OZONE_VOLUME
 rm -rf "${OZONE_VOLUME}"
 mkdir -p "${OZONE_VOLUME}"/{dn1,dn2,dn3,dn4,dn5,om1,om2,om3,scm1,scm2,scm3,recon,s3g,kms}
 
+# shellcheck source=/dev/null
+source "$COMPOSE_DIR/../testlib.sh"
+
 if [[ -n "${OZONE_VOLUME_OWNER}" ]]; then
   current_user=$(whoami)
   if [[ "${OZONE_VOLUME_OWNER}" != "${current_user}" ]]; then
@@ -46,8 +49,7 @@ if [[ -n "${OZONE_VOLUME_OWNER}" ]]; then
   fi
 fi
 
-# shellcheck source=/dev/null
-source "$COMPOSE_DIR/../testlib.sh"
+fix_data_dir_permissions
 
 start_docker_env
 
